cec3d48d12a75d6f170ab89950d22f43b5f840a1
Yuval Lavi <Yuvalavi12@gmail.com>
2025-09-05 21:38:01 +0000

    make sure the chat is centered vertically

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col min-h-screen bg-background">
-      <main className="flex-1 container mx-auto p-4 sm:p-6 md:p-8 flex justify-center items-start">
+      <main className="flex-1 container mx-auto p-4 sm:p-6 md:p-8 flex justify-center items-center">
         <div className="w-full max-w-4xl">
             {itinerary || isLoading || error ? (
               <ItineraryDisplay
